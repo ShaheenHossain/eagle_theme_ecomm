32a290d5386782b74cf670cef13802ac7c8e4318
Md. Shaheen Hossain <rapidgrps@princegroup-bd.com>
2020-06-02 03:05:33 +0600

    try to thme ecomm but problem

---
+++ eagle_theme_base/__manifest__.py
@@ -0,0 +1,65 @@
+{
+    'name': "Eagle Theme Base",
+
+    'summary': """
+        Eagle Theme Base""",
+
+    'description': """
+        ecommerce theme,
+        best ecommerce theme,
+        frontend theme,
+        eagle theme,
+        spa theme,
+        beauty theme,
+        fitness theme,
+        corporate theme,
+        business theme,
+        enterprise theme,
+        community theme,
+        community,
+        enterprise,  
+        best enterprise theme,
+        best community theme,
+        best eagle theme,
+        best eagle themes,
+        top eagle theme,
+        top frontend eagle theme,
+        best frontend eagle theme,
+        clarico,
+        alan,
+        number 1 eagle theme,
+        number 1 theme,
+        beautiful eagle theme,
+        stunning eagle theme,
+        eagle apps,
+    """,
+
+    'author': "Ksolves India Pvt. Ltd.",
+    'website': "https://www.eagle-erp.com/",
+    'license': 'OPL-1',
+    'currency': 'EUR',
+    'price': '30.0',
+    'category': 'tools',
+    'version': '2.0.2',
+
+    'depends': ['base', 'website_blog', 'sale_management', 'website_sale', 'website_theme_install', 'rating'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/ks_product_category.xml',
+        'views/ks_product_tags.xml',
+        'views/ks_product_grid.xml',
+        'views/ks_product_slider.xml',
+        'views/ks_main_slider.xml',
+        'views/ks_brand_view.xml',
+        'data/ks_featured.xml',
+    ],
+    'images': [
+        'static/description/banner.png',
+    ],
+    "application": True,
+
+}
+# -*- coding: utf-8 -*-
